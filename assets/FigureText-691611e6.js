de687cad3a6822f4087ec8f1d042e1edf9f01603
Ahren Chen <ahren@hotmail.ca>
2024-08-18 22:34:37 -0400

    Deploy to GitHub Pages

---
+++ assets/FigureText-691611e6.js
@@ -0,0 +1,1 @@
+import{_ as e,C as r,D as a,E as s,F as o,M as c}from"./index-ee6f11e4.js";import{d as i}from"./VCard-f157f250.js";const n={name:"FigureText",props:{text:{type:String,required:!0}}};function p(x,_,t,d,f,u){return r(),a(i,{class:"font-weight-light"},{default:s(()=>[o(c(t.text),1)]),_:1})}const m=e(n,[["render",p]]);export{m as F};
