bd22e6f69cca23c2546b5317dbf9808d7b105caa
Ahren Chen <ahren@hotmail.ca>
2024-08-18 22:10:59 -0400

    More testing

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'Deploy to GitHub Pages'
 
 # Push the build to the gh-pages branch of your GitHub repo
-git push -f https://github.com/Ahren-Chen/Ahren-Chen.github.io.git master:gh-pages
+git push -f git@github.com:Ahren-Chen/Ahren-Chen.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# if you are deploying to https://Ahren-Chen.github.io/<REPO>
+# git push -f git@github.com:Ahren-Chen/<REPO>.git main:gh-pages
 
 cd -
